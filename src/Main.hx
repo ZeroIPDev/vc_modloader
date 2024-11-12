e457df3cb36b817559e4258a142330d60317b0af
CCC200 <11540770+CCC200@users.noreply.github.com>
2024-11-12 14:10:39 -0500

    Load mod list + select

--- src/Main.hx
+++ src/Main.hx
@@ -5,12 +5,23 @@ import sys.FileSystem;
 import sys.io.File;
 import haxe.xml.Access;
 
+typedef Mod = {
+    var name:String;
+    var description:String;
+    var content:String;
+}
+
 class Main {
 
     public static var content:String;
+    public static var modlist:Array<Mod>;
+
+    private static var _xml:Access;
+    private static inline var XML_PATH:String = "META-INF/AIR/application.xml";
 
     public static function main() {
         getAppData();
+        getModList();
         var app = new HaxeUIApp();
         app.ready(function() {
             app.addComponent(new MainView());
@@ -19,8 +30,7 @@ class Main {
     }
 
     public static function getAppData() {
-        var xml_exists = FileSystem.exists(XML_PATH);
-        if(!xml_exists) {
+        if(!FileSystem.exists(XML_PATH)) {
             throw("FATAL: application.xml does not exist!");
             Sys.exit(1);
         }
@@ -29,7 +39,23 @@ class Main {
         content = _xml.node.initialWindow.node.content.innerData;
     }
 
-    //Private
-    private static var XML_PATH:String = "META-INF/AIR/application.xml";
-    private static var _xml:Access;
+    public static function getModList() {
+        modlist = new Array<Mod>();
+        if(!FileSystem.exists("mods")) {
+            trace("Mod folder does not exist");
+            return;
+        }
+        var dir = FileSystem.readDirectory("mods");
+        for(i in 0...dir.length) {
+            var _path = "mods/" + dir[i];
+            if(!FileSystem.isDirectory(_path)) continue;
+            var _configpath = _path + "/mod.json";
+            if(!FileSystem.exists(_configpath)) continue;
+            var _string = File.getContent(_configpath);
+            var _mod:Mod = haxe.Json.parse(_string);
+            _mod.content = _path + "/" + _mod.content;
+            modlist.push(_mod);
+        }
+    }
+
 }
--- src/MainView.hx
+++ src/MainView.hx
@@ -8,22 +8,33 @@ import haxe.ui.events.UIEvent;
 @:build(haxe.ui.ComponentBuilder.build("assets/main-view.xml"))
 class MainView extends VBox {
 
-    public var NO_MOD_STRING:String = "The default game with no modifications.";
+    public var mod_select:String;
+    
+    private static inline var NO_MOD_CONTENT:String = "VC.swf";
+    private static inline var NO_MOD_STRING:String = "The default game with no modifications.";
 
     public function new() {
         super();
-        var option = new OptionBox();
-        option.registerEvent(UIEvent.CHANGE, function(e) {
-            setDescription("A test mod (it's not real).");
-        });
-        option.id = "test";
-        option.text = "Test";
-        
-        modlistview.addComponent(option);
-        option.selected = true;
+        setModList();
     }
 
-    public function setDescription(dtext:String) {
+    public function setModList() {
+        if(Main.content == NO_MOD_CONTENT) NoMod.selected = true;
+
+        for(i in 0...Main.modlist.length) {
+            var entry = new OptionBox();
+            entry.registerEvent(UIEvent.CHANGE, function(e) {
+                setModSelect(Main.modlist[i].content, Main.modlist[i].description);
+            });
+            entry.id = Main.modlist[i].content;
+            entry.text = Main.modlist[i].name;
+            modlistview.addComponent(entry);
+            if(Main.content == entry.id) entry.selected = true;
+        }
+    }
+
+    public function setModSelect(content:String, dtext:String) {
+        mod_select = content;
         description.text = dtext;
     }
     
