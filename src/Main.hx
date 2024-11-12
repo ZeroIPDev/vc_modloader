9d8d07b6099ee77c470b9d047271ac68a666b121
CCC200 <11540770+CCC200@users.noreply.github.com>
2024-11-12 00:09:51 -0500

    Add main XML reading/data capture

--- src/Main.hx
+++ src/Main.hx
@@ -1,14 +1,35 @@
 package ;
 
 import haxe.ui.HaxeUIApp;
+import sys.FileSystem;
+import sys.io.File;
+import haxe.xml.Access;
 
 class Main {
+
+    public static var content:String;
+
     public static function main() {
+        getAppData();
         var app = new HaxeUIApp();
         app.ready(function() {
             app.addComponent(new MainView());
-
             app.start();
         });
     }
+
+    public static function getAppData() {
+        var xml_exists = FileSystem.exists(XML_PATH);
+        if(!xml_exists) {
+            throw("FATAL: application.xml does not exist!");
+            Sys.exit(1);
+        }
+        var _string = File.getContent(XML_PATH);
+        _xml = new Access(Xml.parse(_string).firstElement());
+        content = _xml.node.initialWindow.node.content.innerData;
+    }
+
+    //Private
+    private static var XML_PATH:String = "META-INF/AIR/application.xml";
+    private static var _xml:Access;
 }
